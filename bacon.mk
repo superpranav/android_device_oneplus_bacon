2fa57678d415da434fc40dd58a1c3225d3f6341d
Adin Kwok <adin.kwok@gmail.com>
2016-10-18 19:27:33 -0700

    Revert "bacon: build omni's SnapdragonCamera"
    This reverts commit 8c0e0c3540d475f8074789c8ccb2188670837159.

--- bacon.mk
+++ bacon.mk
@@ -96,8 +96,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.bacon \
-    SnapdragonCamera
+    camera.bacon
 
 PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.codecremote=false
