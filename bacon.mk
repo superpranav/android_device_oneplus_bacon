052c96f78fcdb9a30810d7bea0454d25f1e13f05
superpranav <superpranav@users.noreply.github.com>
2016-11-09 22:58:43 +0530

    Camera should work now after this

--- bacon.mk
+++ bacon.mk
@@ -99,9 +99,6 @@ PRODUCT_PACKAGES += \
     camera.bacon \
     Snap
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    media.stagefright.codecremote=false
-
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
